7a5f3cbb7f948f3cafd4987eb6aaac78f24f414e
Daan <37297387+dspieard@users.noreply.github.com>
2021-05-25 13:28:14 +0200

    update

--- scripts/basicsscript.sh
+++ scripts/basicsscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install apache2
-touch /etc/security/temp.bak
+apt-get -y install nginx
+touch /etc/security/notes.docx
 cp /vagrant/webpage/index.html /var/www/html/index.html
